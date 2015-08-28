4112330ba048eaf0cffb9e79ba2ec78ea1a198ad
Tamas Kenez <tamas.kenez@kishonti.net>
2015-08-28 14:12:13 +0200

    abbrev.h initial commit with BEGINEND

---
+++ sx/abbrev.h
@@ -0,0 +1,9 @@
+#ifndef ABBREV_INCLUDED_2704934
+#define ABBREV_INCLUDED_2704934
+
+namespace sx {
+    #define BEGINEND(X) ((X).begin()), ((X).end())
+    //#define BEGINEND(X) (begin(X)), (end(X))
+}
+
+#endif
